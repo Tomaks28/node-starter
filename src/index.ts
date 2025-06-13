583965fabd73c1d56086c3d0bd0fec424322aea8
Thomas <152863307+Thomas-TF1@users.noreply.github.com>
2025-06-13 17:09:44 +0200

    feat(eslint): lint project files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import moduleAlias from "module-alias";
 import path from "path";
+
+import moduleAlias from "module-alias";
 
 const baseDir = process.env.NODE_ENV === "production" ? "dist" : "src";
 moduleAlias.addAlias("@", path.resolve(__dirname, "..", baseDir));
