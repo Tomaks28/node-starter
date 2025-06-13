a58b8dd732374b60ae67911c2c743b5b43f3d1c7
Thomas <152863307+Thomas-TF1@users.noreply.github.com>
2025-06-13 16:57:05 +0200

    feat(eslint): add lint script in package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
+
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
