5842008645b99d4a3046ece2aa58ffa1e5adf792
Thomas <152863307+Thomas-TF1@users.noreply.github.com>
2025-06-13 17:09:34 +0200

    feat(eslint): add import/order rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,36 @@
-import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import eslintPluginImport from "eslint-plugin-import";
 import globals from "globals";
-
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
+    plugins: {
+      js,
+      import: eslintPluginImport,
+    },
     extends: ["js/recommended"],
+    rules: {
+      // 🧹 Classement des imports
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+          "newlines-between": "always",
+        },
+      ],
+    },
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
