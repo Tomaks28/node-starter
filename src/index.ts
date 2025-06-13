5488179a62d66cd108b369729fb7bf37c8f6b250
Thomas <152863307+Thomas-TF1@users.noreply.github.com>
2025-06-13 17:11:28 +0200

    feat(eslint): use of console.info instead of console.log

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ import moduleAlias from "module-alias";
 const baseDir = process.env.NODE_ENV === "production" ? "dist" : "src";
 moduleAlias.addAlias("@", path.resolve(__dirname, "..", baseDir));
 
-console.log(`Welcome to node starter!`);
+console.info(`Welcome to node starter!`);
